c9a19d2ed87814f755c544de2662b96cc3b91e00
root <root@LENOVO.localdomain>
2020-04-13 21:46:47 -0400

    Agregamos Modificaciones en Modulos con rutas

---
+++ src/app/pages/pages.module.ts
@@ -0,0 +1,29 @@
+
+import { PAGES_ROUTES } from './pages.routes';
+import { NgModule } from '@angular/core';
+import { SharedModule } from '../shared/shared.module';
+import { PagesComponent } from './pages.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { Graficas1Component } from './graficas1/graficas1.component';
+import { ProgressComponent } from './progress/progress.component';
+
+@NgModule({
+
+    declarations: [
+        PagesComponent,
+        DashboardComponent,
+        ProgressComponent,
+        Graficas1Component
+    ],
+    exports: [
+        DashboardComponent,
+        ProgressComponent,
+        Graficas1Component
+    ],
+    imports: [
+        SharedModule,
+        PAGES_ROUTES
+    ]
+})
+
+export class PagesModule { }
